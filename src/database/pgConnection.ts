7628b19a765bcae3ef05f68f5cabe6dc7bf980dc
Dinith <dinithshenuka@gmail.com>
2025-11-09 00:48:46 +0530

    refactor: clean up pgConnection.ts by removing test query
    - Remove the test query from database connection file
    - Keep only the connection setup and db export
    - Prepare for future database operations

--- src/database/pgConnection.ts
+++ src/database/pgConnection.ts
@@ -4,12 +4,4 @@ import { env } from '@/common/utils/envConfig';
 const pgp = pgPromise(/* options */);
 const db = pgp(env.DATABASE_URL);
 
-db.one('SELECT $1 AS value', 123)
-  .then((data) => {
-    console.log('DATA:', data.value);
-  })
-  .catch((error) => {
-    console.log('ERROR:', error);
-  });
-
 export { db };
