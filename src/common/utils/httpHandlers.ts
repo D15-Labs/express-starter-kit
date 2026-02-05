e799554fba37f5a5d222f36cc8b01f0a76e3b098
Dinith <dinithshenuka@gmail.com>
2026-02-05 12:16:59 +0530

    fix: update Zod imports for v4 compatibility

--- src/common/utils/httpHandlers.ts
+++ src/common/utils/httpHandlers.ts
@@ -1,15 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import type { ZodError, ZodSchema } from "zod";
+import { z } from "zod";
 
 import { ServiceResponse } from "@/common/models/serviceResponse";
 
-export const validateRequest = (schema: ZodSchema) => async (req: Request, res: Response, next: NextFunction) => {
+export const validateRequest = (schema: z.ZodTypeAny) => async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		req.body = schema.parse(req.body);
 		next();
 	} catch (err) {
-		const error = err as ZodError;
+		const error = err as z.ZodError;
 		const statusCode = StatusCodes.BAD_REQUEST;
 		const serviceResponse = ServiceResponse.failure(`Validation error: ${error.message}`, null, statusCode);
 		res.status(serviceResponse.statusCode).send(serviceResponse);
